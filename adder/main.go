892081517ddeb3148950f1c5c9cf384dbf7ed5c8
Adam Reese <adam@reese.io>
2024-07-01 20:21:28 -0700

    Add write to stdout

--- adder/main.go
+++ adder/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "github.com/adamreese/component-example/adder/docs/adder/v0.1.0/add"
+import (
+	"os"
+
+	"github.com/adamreese/component-example/adder/docs/adder/v0.1.0/add"
+)
+
+func init() {
+	// never panics
+	panic("oh, snap")
+}
 
 func init() {
 	add.Exports.Add = func(a, b int32) int32 {
+		os.Stdout.WriteString("hello") // this causes a panic
+
 		return a + b
 	}
 }
